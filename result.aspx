221142a471d5c8f2b9e0b891cb6de38427078c14
respag <respag@hotmail.com>
2015-03-15 16:10:35 -0500

    Primer Commit

---
+++ result.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="result.aspx.cs"
+    Inherits="MensajesDeError.result" %>
+<!DOCTYPE html>
+<html>
+<head runat="server">
+  <meta charset="utf-8" />
+  <title>Muestra resultados</title>
+</head>
+<body>
+  <form id="form1" runat="server">
+    <asp:Label ID="Label1" runat="server"></asp:Label>
+  </form>
+</body>
+</html>
---
+++ result.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MensajesDeError
+{
+    public partial class result : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var nombre = Request.QueryString["txtNombre"].ToString();
+            var edad = Request.QueryString["txtEdad"].ToString();
+            var email = Request.QueryString["txtEmail"].ToString();
+            Label1.Text = String.Format("Su nombre es {0}, ha ingresado un password válido, " +
+                               "tiene {1} años de edad y su email es {2}", nombre, edad, email);
+        }
+    }
+}
